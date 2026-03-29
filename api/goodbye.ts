6340bd4f67634356a73b2a4f149bd6616dce359c
Artem Nosachenko <blaider123@gmail.com>
2026-03-29 17:18:05 +0300

    29.03.26 | upd 2 | api-folder index

--- api/goodbye.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-const log = console.log;
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json({
-    message: `Goodbye, Human!`,
-  });
-}
--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,12 @@
-// @ts-ignore
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import fs   from "fs/promises";
+import path from "path";
 const log = console.log;
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return res.json({
-    message: `<h1>I N D E X . T S</h1>>`,
-  });
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    const file = path.join(process.cwd(), "api.html");
+    const html = await fs.readFile(file, "utf8");
+
+    return res.status(200).send(html);
+    // return res.json({ message: `msg` });
 }
