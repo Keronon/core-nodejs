71a0e8c30551909e5fe7fe1833a53d9b05396ade
Artem Nosachenko <blaider123@gmail.com>
2026-03-29 17:54:14 +0300

    29.03.26 | tst 5 | api-folder index

--- api/index.ts
+++ api/index.ts
@@ -7,7 +7,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     //const file = path.join(process.cwd(), "api", "api.html");
     //const html = await fs.promises.readFile(file, "utf8");
 
-    return res.status(200).send(printDirStructure('../')); // process.cwd()
+    return res.status(200).send(printDirStructure('./')); // process.cwd()
     // return res.json({ message: `msg` });
 }
 
